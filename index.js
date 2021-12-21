03d532926c78933d3ebc0e447cb72bba232d4839
Moharram Moharrami <moharram.m98@gmail.com>
2021-12-21 17:49:55 +0330

    add email address to interactive page

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ emailCollectorForm.addEventListener("submit", event => {
 
     let ourFormData = new FormData(event.target);
     let userFirstName = ourFormData.get("firstName");
+    let userEmailAddress = ourFormData.get("emailAddress")
 
     let updateHtmlContent = `
         <h2>Congratulations, ${userFirstName}!</h2>
 
         <p>You're on your way to becoming a BBQ master!</p>
 
-        <p class="fine-print">We'll never share your information without your permission</p>
+        <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>
     `
 
     let ourMainContent = document.getElementById("Main-Content");
